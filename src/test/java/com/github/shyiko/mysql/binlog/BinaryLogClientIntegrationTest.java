e1a873df8767e3cb23b2574c700c5da09fa6bd8b
Georgie <george@fivetran.com>
2015-12-03 19:07:21 -0800

    Configure java.util.logging

--- src/test/java/com/github/shyiko/mysql/binlog/BinaryLogClientIntegrationTest.java
+++ src/test/java/com/github/shyiko/mysql/binlog/BinaryLogClientIntegrationTest.java
@@ -96,7 +96,7 @@ public class BinaryLogClientIntegrationTest extends AbstractIntegrationTest {
 
     protected static final long DEFAULT_TIMEOUT = TimeUnit.SECONDS.toMillis(3);
 
-    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
+    private final Logger logger = Logger.getLogger("donkey");
 
     {
         logger.setLevel(Level.FINEST);
