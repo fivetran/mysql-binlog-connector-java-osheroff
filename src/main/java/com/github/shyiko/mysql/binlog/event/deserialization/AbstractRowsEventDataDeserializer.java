f518fb1582bc00ae36181603159093edc989b0b4
glarwood <gabriel@fivetran.com>
2020-04-01 16:32:05 -0700

    fix(AbstractRowsEventDataDeserializer): add microsecond precision for time

--- src/main/java/com/github/shyiko/mysql/binlog/event/deserialization/AbstractRowsEventDataDeserializer.java
+++ src/main/java/com/github/shyiko/mysql/binlog/event/deserialization/AbstractRowsEventDataDeserializer.java
@@ -22,6 +22,7 @@ import com.github.shyiko.mysql.binlog.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.sql.Time;
 import java.util.BitSet;
 import java.util.Calendar;
 import java.util.Map;
@@ -285,7 +286,7 @@ public abstract class AbstractRowsEventDataDeserializer<T extends EventData> imp
         if (deserializeDateAndTimeAsLong) {
             return castTimestamp(timestamp, 0);
         }
-        return timestamp != null ? new java.sql.Time(timestamp) : null;
+        return timestamp != null ? new java.sql.Timestamp(timestamp) : null;
     }
 
     protected Serializable deserializeTimeV2(int meta, ByteArrayInputStream inputStream) throws IOException {
@@ -313,7 +314,7 @@ public abstract class AbstractRowsEventDataDeserializer<T extends EventData> imp
         if (deserializeDateAndTimeAsLong) {
             return castTimestamp(timestamp, fsp);
         }
-        return timestamp != null ? new java.sql.Time(timestamp) : null;
+        return timestamp != null ? convertLongTimestamptWithFSP(timestamp, fsp) : null;
     }
 
     protected Serializable deserializeTimestamp(ByteArrayInputStream inputStream) throws IOException {
